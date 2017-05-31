00178a109780286cf87ad1c17c4e8de6ec611502
cw0925 <294760125@qq.com>
2017-05-31 18:33:11 +0800

    我的界面的初步实现

---
+++ MeiTuan/Classes/My/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  MeiTuan
+//
+//  Created by afarsoft on 2017/5/31.
+//  Copyright © 2017年 CW. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- MeiTuan/Classes/My/MyViewController.swift
+++ MeiTuan/Classes/My/MyViewController.swift
@@ -8,14 +8,55 @@
 
 import UIKit
 
-class MyViewController: BaseViewController {
+class MyViewController: BaseViewController,UITableViewDelegate,UITableViewDataSource{
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        initUI()
     }
-
+    func initUI()  {
+        let head = UIView.init(frame: CGRect(x: 0, y: 0, width:SCREENW, height: 100))
+        head.backgroundColor = UIColor.green
+        
+        let icon  = UIImageView.init(frame: CGRect(x: 20, y: 20, width:60, height: 60))
+        icon.image = UIImage.init(named: "icon_homepage_movieCategory_44x44_")
+        head.addSubview(icon)
+        
+        let name = UILabel.init(frame: CGRect(x: 100, y: 20, width:SCREENW-100, height: 25))
+        name.text = "cw0925"
+        head.addSubview(name)
+        
+        let info = UILabel.init(frame: CGRect(x: 100, y: 45, width:SCREENW-100, height: 30))
+        info.text = "个人信息>"
+        head.addSubview(info)
+        
+        
+        let myTable = UITableView.init(frame: view.bounds, style: .grouped)
+        myTable.delegate = self
+        myTable.dataSource = self
+        myTable.tableHeaderView = head
+        view.addSubview(myTable)
+        myTable.register(UINib.init(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "myCell")
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 4
+        }else if section == 1{
+            return 5
+        }else{
+            return 3
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
